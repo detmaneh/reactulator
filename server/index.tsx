69340dd7505bb2b6e096a67abf833a6f293c941b
detmaneh <detmaneh@gmail.com>
2025-05-26 17:26:46 -0500

    updating packages

--- server/index.tsx
+++ server/index.tsx
@@ -2,59 +2,55 @@
 const path = require('path');
 const express = require("express");
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 6040;
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use(express.static(path.resolve("/", '/ui/build')));
-app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.resolve("/", '/ui/build', 'index.html'));
-});
-
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
-
-app.get("/api", (req, res) => {
-  res.json({ message: "CalcServer Conected" });
-});
-
+const getDigitNumber = (value) => {
+  if (value % 1 !== 0) {
+    return value.toFixed(4);
+  }
+  return value;
+}
 
 app.post("/sub", (req, res) => {
   let nums = req.body;
   let response = getDigitNumber(Number(Number(nums.num1) - Number(nums.num2)));
-  console.log("sub response:"+response + " from "+ nums.num1 + " "+ nums.num2);
+  console.log("sub response:" + response + " from " + nums.num1 + " " + nums.num2);
   res.json({ message: response });
 });
 
 app.post("/sum", (req, res) => {
   let nums = req.body;
   let response = getDigitNumber(Number(Number(nums.num1) + Number(nums.num2)));
-  console.log("SUM response:"+response + " from "+ nums.num1 + " "+ nums.num2);
+  console.log("sum response:" + response + " from " + nums.num1 + " " + nums.num2);
   res.json({ message: response });
 });
 
 app.post("/div", (req, res) => {
   let nums = req.body;
   let response = getDigitNumber(Number(Number(nums.num1) / Number(nums.num2)));
-  console.log("div response:"+response + " from "+ nums.num1 + " "+ nums.num2);
+  console.log("div response:" + response + " from " + nums.num1 + " " + nums.num2);
   res.json({ message: response });
 });
 
 app.post("/mul", (req, res) => {
   let nums = req.body;
   let response = getDigitNumber(Number(Number(nums.num1) * Number(nums.num2)));
-  console.log("mul response:"+response + " from "+ nums.num1 + " "+ nums.num2);
+  console.log("mul response:" + response + " from " + nums.num1 + " " + nums.num2);
   res.json({ message: response });
 });
 
 app.post("/perc", (req, res) => {
   let nums = req.body;
-  let response = getDigitNumber(Number(Number(nums.num2) * Number( ( Number(nums.num1) / 100))));
-  console.log("perc response:"+response + " from "+ nums.num1 + " "+ nums.num2);
+  let response = getDigitNumber(Number(Number(nums.num2) * Number((Number(nums.num1) / 100))));
+  console.log("perc response:" + response + " from " + nums.num1 + " " + nums.num2);
   res.json({ message: response });
+});
+
+app.get("/", (req, res) => {
+  res.json({ message: "CalcServer Conected" });
 });
 
 app.listen(PORT, () => {
@@ -62,10 +58,18 @@ app.listen(PORT, () => {
 });
 
 
-const getDigitNumber = (value) => {
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
-    if(value % 1 !== 0) {
-        return value.toFixed(4); 
-    }
-    return value; 
-}
+
+app.use(express.static(path.join(__dirname, '/ui/build')));
+
+app.use(express.static(path.resolve(__dirname, '/ui/build')));
+app.get(/(.*)/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, '/ui/build', 'index.html'));
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Sorry, the requested resource was not found.');
+});
